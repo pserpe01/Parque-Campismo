a19b3bc4b9165d958c2de02d76ac4486c39c80c8
T1503 <araujotomas76@gmail.com>
2020-12-30 00:51:32 +0000

    DAsÈ

---
+++ AFPcampismo/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void menu()
+{
+    int op;
+    printf(" ______________________________________ \n");
+    printf("|                                      |\n");
+    printf("|    Gestao do parque de  Campismo     |\n");
+    printf("|______________________________________|\n");
+    printf("| 1 - Ver o mapa de lotes              |\n");
+    printf("| 2 - Reservar Lote                    |\n");
+    printf("| 3 - Consultar Lote                   |\n");
+    printf("| 4 - Pesquisar um lote por campista   |\n");
+    printf("| 5 - Editar um lote reservado         |\n");
+    printf("| 6 - Libertar um lote                 |\n");
+    printf("| 7 - Sair                             |\n");
+    printf("|______________________________________|\n");
+    printf("\n Opcao:");
+
+}
+
+int main()
+{
+    menu();
+
+    return 0;
+}
